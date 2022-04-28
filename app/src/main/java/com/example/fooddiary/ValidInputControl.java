8f0c67622f2bc4ef6189760a31adce68d1dede05
Opilane TTHK <opilane@tthk.ee>
2022-04-28 13:07:00 +0300

    add password pattern matcher method

--- app/src/main/java/com/example/fooddiary/ValidInputControl.java
+++ app/src/main/java/com/example/fooddiary/ValidInputControl.java
@@ -1,2 +1,42 @@
-package com.example.fooddiary;public class ValidInputControl {
+package com.example.fooddiary;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidInputControl {
+
+    public static boolean isValidEmail(String email){
+        Pattern pattern;
+        Matcher matcher;
+
+        final String EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-zA-Z]+\\.+[a-z]+";
+
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+
+        return matcher.matches();
+
+    }
+
+    public static boolean  isValidPassword(String password){
+        Pattern pattern;
+        Matcher matcher;
+
+
+        final String PASSWORD_PATTERN =
+                "^" +
+                        "(?=.*[0-9])" +         //at least 1 digit
+                        "(?=.*[a-z])" +         //at least 1 lower case letter
+                        "(?=.*[A-Z])" +         //at least 1 upper case letter
+                        "(?=\\S+$)" +           //no white spaces
+                        ".{4,20}" +               //at least 4 characters to 20
+                        "$";
+        pattern =  Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+
+        return matcher.matches();
+    }
+
+
 }
