bff4956dfa349d2f9b4b04a5d3de327232af69eb
hendris <maarhendris@gmail.com>
2022-05-12 10:17:12 +0300

    Polished other aspects of the project (such as making password input hide password, add more strings to strings.xml, IDs being more consistent, appearance of fragments)

--- app/src/main/java/com/example/fooddiary/ValidInputControl.java
+++ app/src/main/java/com/example/fooddiary/ValidInputControl.java
@@ -26,10 +26,10 @@ public class ValidInputControl {
         final String PASSWORD_PATTERN =
                 "^" +
                         "(?=.*[0-9])" +         //at least 1 digit
-                        "(?=.*[a-z])" +         //at least 1 lower case letter
-                        "(?=.*[A-Z])" +         //at least 1 upper case letter
-                        "(?=\\S+$)" +           //no white spaces
-                        ".{4,20}" +               //at least 4 characters to 20
+                        "(?=.*[a-z])" +         //at least 1 lowercase letter
+                        "(?=.*[A-Z])" +         //at least 1 uppercase letter
+                        "(?=\\S+$)" +           //no whitespaces
+                        ".{8,20}" +               //at least 8 to 20 characters
                         "$";
         pattern =  Pattern.compile(PASSWORD_PATTERN);
         matcher = pattern.matcher(password);
