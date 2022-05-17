92568e82554e868e7a81eeceb84991c05321b5c9
Hegert Taresalu <herkuti@gmail.com>
2022-05-17 08:45:29 +0300

    add logout

--- app/src/main/java/com/example/fooddiary/fragment/VerifiedFragment.java
+++ app/src/main/java/com/example/fooddiary/fragment/VerifiedFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
@@ -14,16 +15,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.fooddiary.R;
+import com.example.fooddiary.ViewModel.LoginViewModel;
 
 public class VerifiedFragment extends Fragment implements View.OnClickListener {
 
     NavController navController;
     Button btnCalendar;
+    LoginViewModel loginViewModel;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_verified, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
     }
 
     @Override
@@ -33,6 +44,8 @@ public class VerifiedFragment extends Fragment implements View.OnClickListener {
         view.findViewById(R.id.btnCalendar).setOnClickListener(this);
     }
 
+
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -40,6 +53,10 @@ public class VerifiedFragment extends Fragment implements View.OnClickListener {
                 navController.navigate(R.id.action_verifiedFragment_to_calendarFragment);
                 break;
             case  R.id.log_out:
+                loginViewModel.logout();
+
+
+
 
         }
     }
